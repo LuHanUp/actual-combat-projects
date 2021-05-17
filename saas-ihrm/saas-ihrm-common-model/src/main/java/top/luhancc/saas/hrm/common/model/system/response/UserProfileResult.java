b926696f9a2fc2501ea6fda3a04cc53368d33113
luhan <765478939@qq.com>
2021-05-17 10:23:14 +0800

    Saas-iHRM人力资源管理系统：根据token获取用户信息

---
+++ saas-ihrm/saas-ihrm-common-model/src/main/java/top/luhancc/saas/hrm/common/model/system/response/UserProfileResult.java
@@ -0,0 +1,62 @@
+package top.luhancc.saas.hrm.common.model.system.response;
+
+import lombok.Data;
+import top.luhancc.saas.hrm.common.model.system.Permission;
+import top.luhancc.saas.hrm.common.model.system.Role;
+import top.luhancc.saas.hrm.common.model.system.User;
+
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.Map;
+import java.util.Set;
+import java.util.stream.Collectors;
+
+/**
+ * 用户轮廓信息
+ *
+ * @author luHan
+ * @create 2021/5/17 10:00
+ * @since 1.0.0
+ */
+@Data
+public class UserProfileResult {
+    private String mobile;
+    private String username;
+    private String company;
+
+    // 包含menus、points、apis
+    private Map<String, Object> roles;
+
+    public UserProfileResult(User user) {
+        this.mobile = user.getMobile();
+        this.username = user.getUsername();
+        this.company = user.getCompanyName();
+        this.roles = new HashMap<>();
+        Set<Role> roles = user.getRoles();
+        Set<String> menus = new HashSet<>();
+        Set<String> points = new HashSet<>();
+        Set<String> apis = new HashSet<>();
+        for (Role role : roles) {
+            Set<Permission> permissions = role.getPermissions();
+
+            // 获取menu类型的权限code
+            menus.addAll(permissions.stream()
+                    .filter(permission -> permission.getType() == 1)
+                    .map(Permission::getCode)
+                    .collect(Collectors.toSet()));
+            // 获取point类型的权限code
+            points.addAll(permissions.stream()
+                    .filter(permission -> permission.getType() == 2)
+                    .map(Permission::getCode)
+                    .collect(Collectors.toSet()));
+            // 获取api类型的权限code
+            apis.addAll(permissions.stream()
+                    .filter(permission -> permission.getType() == 3)
+                    .map(Permission::getCode)
+                    .collect(Collectors.toSet()));
+        }
+        this.roles.put("menus", menus);
+        this.roles.put("points", points);
+        this.roles.put("apis", apis);
+    }
+}
--- saas-ihrm/saas-ihrm-system/src/main/java/top/luhancc/saas/hrm/system/controller/SystemController.java
+++ saas-ihrm/saas-ihrm-system/src/main/java/top/luhancc/saas/hrm/system/controller/SystemController.java
@@ -1,6 +1,9 @@
 package top.luhancc.saas.hrm.system.controller;
 
+import io.jsonwebtoken.Claims;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.util.StringUtils;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -9,9 +12,11 @@ import top.luhancc.hrm.common.domain.Result;
 import top.luhancc.hrm.common.domain.ResultCode;
 import top.luhancc.hrm.common.utils.JwtUtils;
 import top.luhancc.saas.hrm.common.model.system.User;
+import top.luhancc.saas.hrm.common.model.system.response.UserProfileResult;
 import top.luhancc.saas.hrm.system.domain.param.LoginParam;
 import top.luhancc.saas.hrm.system.service.UserService;
 
+import javax.servlet.http.HttpServletRequest;
 import java.util.HashMap;
 import java.util.Map;
 
@@ -23,6 +28,7 @@ import java.util.Map;
 @RestController
 @RequiredArgsConstructor
 @RequestMapping(value = "/sys")
+@Slf4j
 public class SystemController {
     private final UserService userService;
     private final JwtUtils jwtUtils;
@@ -44,5 +50,32 @@ public class SystemController {
         map.put("companyName", user.getCompanyName());
         String token = jwtUtils.createJwt(user.getId(), user.getUsername(), map);
         return Result.success(token);
+    }
+
+    /**
+     * 获取当前登录用户的信息
+     * <p>
+     * 请求头携带token的规则：Authorization:Bearer token字符串
+     *
+     * @return
+     */
+    @RequestMapping(value = "/profile", method = RequestMethod.POST)
+    public Result<UserProfileResult> profile(HttpServletRequest request) {
+        // 从请求头中获取token
+        String authorization = request.getHeader("Authorization");
+        if (StringUtils.isEmpty(authorization)) {
+            log.warn("没有携带请求头Authorization");
+            return Result.error(ResultCode.UNAUTHENTICATED);
+        }
+        String token = authorization.replace("Bearer ", "");
+        Claims claims = jwtUtils.parseJwt(token);
+        if (claims == null) {
+            log.warn("token不正确,无法获取认证信息:{}", token);
+            return Result.error(ResultCode.UNAUTHENTICATED);
+        }
+        // 根据token获取其中的用户id
+        String userId = claims.getId();
+        User user = userService.findById(userId);
+        return Result.success(new UserProfileResult(user));
     }
 }

805c43cd0a46cad583096e404f62a7479bfb8dff
luhan <765478939@qq.com>
2021-05-25 15:52:57 +0800

    Saas-iHRM人力资源管理系统：网关层做鉴权处理，只是验证有没有Authorization请求头

---
+++ saas-ihrm/saas-ihrm-gateway/src/main/java/top/luhancc/ihrm/gateway/filter/LoginFilter.java
@@ -0,0 +1,80 @@
+package top.luhancc.ihrm.gateway.filter;
+
+import com.netflix.zuul.ZuulFilter;
+import com.netflix.zuul.context.RequestContext;
+import com.netflix.zuul.exception.ZuulException;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.cloud.netflix.zuul.filters.support.FilterConstants;
+import org.springframework.http.HttpStatus;
+import org.springframework.stereotype.Component;
+import org.springframework.util.StringUtils;
+
+import javax.servlet.http.HttpServletRequest;
+
+/**
+ * 登录过滤器
+ *
+ * @author luHan
+ * @create 2021/5/25 15:25
+ * @since 1.0.0
+ */
+@Component
+@Slf4j
+public class LoginFilter extends ZuulFilter {
+
+    /**
+     * 定义过滤器类型
+     * <pre>
+     * pre：在执行路由请求之前执行此过滤器
+     * routing：在路由请求时执行此过滤器
+     * post：在routing和error之后执行
+     * error：在请求出现异常后执行此过滤器
+     * </pre>
+     *
+     * @return
+     */
+    @Override
+    public String filterType() {
+        return FilterConstants.PRE_TYPE;
+    }
+
+    /**
+     * 定义过滤器优先级，返回的数字越小，优先级越高
+     *
+     * @return
+     */
+    @Override
+    public int filterOrder() {
+        return 0;
+    }
+
+    /**
+     * 判断此过滤器是否需要执行
+     *
+     * @return true-执行 false-不执行
+     */
+    @Override
+    public boolean shouldFilter() {
+        return true;
+    }
+
+    /**
+     * 执行过滤器处理
+     *
+     * @return
+     * @throws ZuulException
+     */
+    @Override
+    public Object run() throws ZuulException {
+        RequestContext context = RequestContext.getCurrentContext();
+        HttpServletRequest request = context.getRequest();
+        // 从请求头中获取token
+        String token = request.getHeader("Authorization");
+        if (StringUtils.isEmpty(token)) {
+            log.warn("没有携带请求头Authorization");
+            context.setSendZuulResponse(false);
+            context.setResponseStatusCode(HttpStatus.UNAUTHORIZED.value());
+        }
+        return null;
+    }
+}

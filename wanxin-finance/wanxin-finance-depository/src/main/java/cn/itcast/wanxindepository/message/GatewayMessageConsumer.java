fd1b297c5f96e6dea3b242589d2f0adb232d86c5
luhan <765478939@qq.com>
2021-06-11 14:00:50 +0800

    银行存管系统发送消息注释打开，方便完成开户的第三阶段

--- wanxin-finance/wanxin-finance-depository/src/main/java/cn/itcast/wanxindepository/message/GatewayMessageConsumer.java
+++ wanxin-finance/wanxin-finance-depository/src/main/java/cn/itcast/wanxindepository/message/GatewayMessageConsumer.java
@@ -21,54 +21,53 @@ import java.nio.charset.StandardCharsets;
  * <P>
  * 开户网关接口通知回调消息消费者
  * </p>
-
  */
 @Slf4j
 @Component
 public class GatewayMessageConsumer {
 
-	private static final String CONSUMER_GROUP = "CID_DEPOSITORY_GATEWAY_NOTIFY";
+    private static final String CONSUMER_GROUP = "CID_DEPOSITORY_GATEWAY_NOTIFY";
 
-	@Autowired
-	private ConfigService configService;
+    @Autowired
+    private ConfigService configService;
 
-	@Autowired
-	private OkHttpService okHttpService;
+    @Autowired
+    private OkHttpService okHttpService;
 
-	public GatewayMessageConsumer(@Value("${rocketmq.name-server}") String mqNameServer) throws MQClientException {
-		DefaultMQPushConsumer consumer = new DefaultMQPushConsumer(CONSUMER_GROUP);
-		consumer.setNamesrvAddr(mqNameServer);
-		// 消费策略：设置消费地点,从最后一个进行消费
-		consumer.setConsumeFromWhere(ConsumeFromWhere.CONSUME_FROM_LAST_OFFSET);
-		// 订阅主题的哪些标签
-		consumer.subscribe("TP_GATEWAY_NOTIFY", "*");
-		// 注册监听器
-		consumer.registerMessageListener((MessageListenerConcurrently) (msgs, context) -> {
-			try {
-				Message msg = msgs.get(0);
-				String body = new String(msg.getBody(), StandardCharsets.UTF_8);
-				//获取异步通知接口名称
-				String tags = msg.getTags();
+    public GatewayMessageConsumer(@Value("${rocketmq.name-server}") String mqNameServer) throws MQClientException {
+        DefaultMQPushConsumer consumer = new DefaultMQPushConsumer(CONSUMER_GROUP);
+        consumer.setNamesrvAddr(mqNameServer);
+        // 消费策略：设置消费地点,从最后一个进行消费
+        consumer.setConsumeFromWhere(ConsumeFromWhere.CONSUME_FROM_LAST_OFFSET);
+        // 订阅主题的哪些标签
+        consumer.subscribe("TP_GATEWAY_NOTIFY", "*");
+        // 注册监听器
+        consumer.registerMessageListener((MessageListenerConcurrently) (msgs, context) -> {
+            try {
+                Message msg = msgs.get(0);
+                String body = new String(msg.getBody(), StandardCharsets.UTF_8);
+                //获取异步通知接口名称
+                String tags = msg.getTags();
 
-				JSONObject jsonBody = JSON.parseObject(body);
-				//获取P2P平台编号
-				String appCode = jsonBody.getJSONObject("headers").getString("KEYS");
-				//获取消息返回主题内容
-				String response = jsonBody.getString("payload");
-				String result = okHttpService
-						.sendHttpGet(configService.getP2PNotifyUrl(appCode), tags, appCode, response);
-				log.info("Receive Message: Result({}),KEYS({}),Tags({}),Body({})", result, appCode, tags, response);
-				if ("OK".equals(result)) {
-					return ConsumeConcurrentlyStatus.CONSUME_SUCCESS;
-				}
-				return ConsumeConcurrentlyStatus.RECONSUME_LATER;
-			} catch (Exception e) {
-				log.warn(e.getMessage(), e);
-				return ConsumeConcurrentlyStatus.RECONSUME_LATER;
-			}
-		});
-		consumer.start();
-		log.info("GatewayMessageConsumer Listener");
-	}
+                JSONObject jsonBody = JSON.parseObject(body);
+                //获取P2P平台编号
+                String appCode = jsonBody.getJSONObject("headers").getString("KEYS");
+                //获取消息返回主题内容
+                String response = jsonBody.getString("payload");
+                String result = okHttpService
+                        .sendHttpGet(configService.getP2PNotifyUrl(appCode), tags, appCode, response);
+                log.info("Receive Message: Result({}),KEYS({}),Tags({}),Body({})", result, appCode, tags, response);
+                if ("OK".equals(result)) {
+                    return ConsumeConcurrentlyStatus.CONSUME_SUCCESS;
+                }
+                return ConsumeConcurrentlyStatus.RECONSUME_LATER;
+            } catch (Exception e) {
+                log.warn(e.getMessage(), e);
+                return ConsumeConcurrentlyStatus.RECONSUME_LATER;
+            }
+        });
+        consumer.start();
+        log.info("GatewayMessageConsumer Listener");
+    }
 
 }
--- wanxin-finance/wanxin-finance-depository/src/main/java/cn/itcast/wanxindepository/service/UserServiceImpl.java
+++ wanxin-finance/wanxin-finance-depository/src/main/java/cn/itcast/wanxindepository/service/UserServiceImpl.java
@@ -27,115 +27,116 @@ import org.springframework.transaction.annotation.Transactional;
 @Service
 public class UserServiceImpl extends ServiceImpl<UserMapper, User> implements UserService {
 
-	@Autowired
-	private BankUserService bankUserService;
+    @Autowired
+    private BankUserService bankUserService;
 
-	@Autowired
-	private BankCardService bankCardService;
+    @Autowired
+    private BankCardService bankCardService;
 
-	@Autowired
-	private DepositoryBankCardMapper depositoryBankCardMapper;
+    @Autowired
+    private DepositoryBankCardMapper depositoryBankCardMapper;
 
-	@Autowired
-	private BalanceDetailsService balanceDetailsService;
+    @Autowired
+    private BalanceDetailsService balanceDetailsService;
 
-	@Autowired
-	private GatewayMessageProducer producer;
+    @Autowired
+    private GatewayMessageProducer producer;
 
-	@Autowired
-	private RequestDetailsService requestDetailsService;
+    @Autowired
+    private RequestDetailsService requestDetailsService;
 
-	@Override
-	@Transactional
-	public PersonalRegisterResponse createUser(PersonalRegisterRequest personalRegisterRequest) {
-		String requestNo = personalRegisterRequest.getRequestNo();
-		PersonalRegisterResponse response = new PersonalRegisterResponse();
-		response.setRequestNo(requestNo);
-		response.setBankCode(personalRegisterRequest.getBankCode());
-		response.setBankName(personalRegisterRequest.getBankName());
+    @Override
+    @Transactional
+    public PersonalRegisterResponse createUser(PersonalRegisterRequest personalRegisterRequest) {
+        String requestNo = personalRegisterRequest.getRequestNo();
+        PersonalRegisterResponse response = new PersonalRegisterResponse();
+        response.setRequestNo(requestNo);
+        response.setBankCode(personalRegisterRequest.getBankCode());
+        response.setBankName(personalRegisterRequest.getBankName());
 
-		//校验个人信息
-		BankUser bankUser = bankUserService
-				.getUser(personalRegisterRequest.getFullname(), personalRegisterRequest.getIdNumber());
-		if (bankUser == null) {
-			throw new BusinessException(LocalReturnCode.E_200301.getDesc());
-		}
+        //校验个人信息
+        BankUser bankUser = bankUserService
+                .getUser(personalRegisterRequest.getFullname(), personalRegisterRequest.getIdNumber());
+        if (bankUser == null) {
+            throw new BusinessException(LocalReturnCode.E_200301.getDesc());
+        }
 
-		//校验银行实体卡
-		BankCard bankCard = new BankCard();
-		bankCard.setUserId(bankUser.getId());
-		bankCard.setBankCode(personalRegisterRequest.getBankCode());
-		bankCard.setCardNumber(personalRegisterRequest.getCardNumber());
-		if (!bankCardService.verify(bankCard)) {
-			throw new BusinessException(LocalReturnCode.E_200103);
-		}
+        //校验银行实体卡
+        BankCard bankCard = new BankCard();
+        bankCard.setUserId(bankUser.getId());
+        bankCard.setBankCode(personalRegisterRequest.getBankCode());
+        bankCard.setCardNumber(personalRegisterRequest.getCardNumber());
+        if (!bankCardService.verify(bankCard)) {
+            throw new BusinessException(LocalReturnCode.E_200103);
+        }
 
-		try {
-			//保存开户信息
-			User user = new User();
-			BeanUtils.copyProperties(personalRegisterRequest, user);
-			user.setIsBindCard(1);
-			user.setAuthList("ALL");
-			save(user);
+        try {
+            //保存开户信息
+            User user = new User();
+            BeanUtils.copyProperties(personalRegisterRequest, user);
+            user.setIsBindCard(1);
+            user.setAuthList("ALL");
+            save(user);
 
-			//保存绑定银行卡信息
-			bankCard = bankCardService.getByCardNumber(personalRegisterRequest.getCardNumber());
-			DepositoryBankCard depositoryBankCard = new DepositoryBankCard();
-			depositoryBankCard.setAppCode(personalRegisterRequest.getAppCode());
-			BeanUtils.copyProperties(bankCard, depositoryBankCard);
-			depositoryBankCard.setMobile(user.getMobile());
-			depositoryBankCard.setUserId(user.getId());
-			depositoryBankCard.setRequestNo(user.getRequestNo());
-			depositoryBankCardMapper.insert(depositoryBankCard);
+            //保存绑定银行卡信息
+            bankCard = bankCardService.getByCardNumber(personalRegisterRequest.getCardNumber());
+            DepositoryBankCard depositoryBankCard = new DepositoryBankCard();
+            depositoryBankCard.setAppCode(personalRegisterRequest.getAppCode());
+            BeanUtils.copyProperties(bankCard, depositoryBankCard);
+            depositoryBankCard.setMobile(user.getMobile());
+            depositoryBankCard.setUserId(user.getId());
+            depositoryBankCard.setRequestNo(user.getRequestNo());
+            depositoryBankCardMapper.insert(depositoryBankCard);
 
-			//设置初始余额信息
-			balanceDetailsService.addForPersonalRegister(
-					new BalanceDetails().setUserNo(user.getUserNo()).setAppCode(personalRegisterRequest.getAppCode())
-							.setRequestContent(JSON.toJSONString(personalRegisterRequest)));
+            //设置初始余额信息
+            balanceDetailsService.addForPersonalRegister(
+                    new BalanceDetails().setUserNo(user.getUserNo()).setAppCode(personalRegisterRequest.getAppCode())
+                            .setRequestContent(JSON.toJSONString(personalRegisterRequest)));
 
-			//更新处理结果
-			response.setSuccess();
-			requestDetailsService.modifyGatewayByRequestNo(response);
+            //更新处理结果
+            response.setSuccess();
+            requestDetailsService.modifyGatewayByRequestNo(response);
 
-			//产生开户成功消息
-			//producer.personalRegister(personalRegisterRequest.getAppCode(), response);
-		} catch (Exception e) {
-			log.error(e.getMessage());
+            //产生开户成功消息
+            producer.personalRegister(personalRegisterRequest.getAppCode(), response);
+        } catch (Exception e) {
+            log.error(e.getMessage());
 
-			//更新处理结果
-			response.setFailure();
-			requestDetailsService.modifyGatewayByRequestNo(response);
+            //更新处理结果
+            response.setFailure();
+            requestDetailsService.modifyGatewayByRequestNo(response);
 
-			//产生开户失败消息
-			//producer.personalRegister(personalRegisterRequest.getAppCode(), response);
-			throw new BusinessException(personalRegisterRequest.getRequestNo(), RemoteReturnCode.EXCEPTION);
-		}
+            //产生开户失败消息
+            producer.personalRegister(personalRegisterRequest.getAppCode(), response);
+            throw new BusinessException(personalRegisterRequest.getRequestNo(), RemoteReturnCode.EXCEPTION);
+        }
 
-		return response;
-	}
+        return response;
+    }
 
-	@Override
-	public DepositoryBankCard getDepositoryBankCardByUserNo(String userNo) {
-		User user = getByUserNo(userNo);
-		return depositoryBankCardMapper.selectOne(
-				Wrappers.<DepositoryBankCard>query().lambda().eq(DepositoryBankCard::getUserId, user.getId()));
-	}
+    @Override
+    public DepositoryBankCard getDepositoryBankCardByUserNo(String userNo) {
+        User user = getByUserNo(userNo);
+        return depositoryBankCardMapper.selectOne(
+                Wrappers.<DepositoryBankCard>query().lambda().eq(DepositoryBankCard::getUserId, user.getId()));
+    }
 
-	@Override
-	public Boolean verifyPassword(String userNo, String password) {
-		User user = getByUserNo(userNo);
-		if (user != null) {
-			return user.getPassword().equals(password);
-		}
-		return false;
-	}
+    @Override
+    public Boolean verifyPassword(String userNo, String password) {
+        User user = getByUserNo(userNo);
+        if (user != null) {
+            return user.getPassword().equals(password);
+        }
+        return false;
+    }
 
-	/**
-	 * 根据用户编号获取用户信息
-	 * @param userNo
-	 * @return
-	 */
-	private User getByUserNo(String userNo) {
-		return getOne(Wrappers.<User>query().lambda().eq(User::getUserNo, userNo), false);
-	}
+    /**
+     * 根据用户编号获取用户信息
+     *
+     * @param userNo
+     * @return
+     */
+    private User getByUserNo(String userNo) {
+        return getOne(Wrappers.<User>query().lambda().eq(User::getUserNo, userNo), false);
+    }
 }

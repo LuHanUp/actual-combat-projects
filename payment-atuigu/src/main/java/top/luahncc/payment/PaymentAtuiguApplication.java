d169a32b325cafdbcac77f26bb14c6ab601d69cf
luhan <765478939@qq.com>
2022-01-08 22:35:39 +0800

    init payment project

---
+++ payment-atuigu/src/main/java/top/luahncc/payment/PaymentAtuiguApplication.java
@@ -0,0 +1,13 @@
+package top.luahncc.payment;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PaymentAtuiguApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PaymentAtuiguApplication.class, args);
+    }
+
+}
---
+++ payment-atuigu/src/test/java/top/luahncc/payment/PaymentAtuiguApplicationTests.java
@@ -0,0 +1,13 @@
+package top.luahncc.payment;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PaymentAtuiguApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}

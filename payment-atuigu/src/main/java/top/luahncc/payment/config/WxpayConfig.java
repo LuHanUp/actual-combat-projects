e26aa39d986ca3bfd3db422b2477b4a028668522
luhan <765478939@qq.com>
2022-01-09 20:25:54 +0800

    fix:获取微信支付的client

--- payment-atuigu/src/main/java/top/luahncc/payment/config/WxpayConfig.java
+++ payment-atuigu/src/main/java/top/luahncc/payment/config/WxpayConfig.java
@@ -1,11 +1,19 @@
 package top.luahncc.payment.config;
 
+import com.wechat.pay.contrib.apache.httpclient.WechatPayHttpClientBuilder;
+import com.wechat.pay.contrib.apache.httpclient.auth.PrivateKeySigner;
+import com.wechat.pay.contrib.apache.httpclient.auth.ScheduledUpdateCertificatesVerifier;
+import com.wechat.pay.contrib.apache.httpclient.auth.WechatPay2Credentials;
+import com.wechat.pay.contrib.apache.httpclient.auth.WechatPay2Validator;
 import com.wechat.pay.contrib.apache.httpclient.util.PemUtil;
 import lombok.Data;
+import org.apache.http.impl.client.CloseableHttpClient;
 import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.PropertySource;
 
+import java.nio.charset.StandardCharsets;
 import java.security.PrivateKey;
 
 
@@ -34,7 +42,7 @@ public class WxPayConfig {
     private String notifyDomain;
 
     /**
-     * 获取私钥文件
+     * 获取商户私钥文件
      *
      * @return
      */
@@ -44,5 +52,38 @@ public class WxPayConfig {
         } catch (Exception e) {
             throw new RuntimeException("私钥文件不存在", e);
         }
+    }
+
+    /**
+     * 获取签名验证器
+     *
+     * <text>@Bean</text> 让这个方法执行一次即可, 基于SpringBean注入的原理使其只执行一次
+     *
+     * @return
+     */
+    @Bean
+    public ScheduledUpdateCertificatesVerifier getVerifier() {
+        // 使用定时更新的签名验证器，不需要传入证书
+        // WechatPay2Credentials:身份认证对象,证明是微信端
+        // PrivateKeySigner:私钥签名对象
+        return new ScheduledUpdateCertificatesVerifier(
+                new WechatPay2Credentials(mchId, new PrivateKeySigner(mchSerialNo, getPrivateKey())),
+                apiV3Key.getBytes(StandardCharsets.UTF_8));
+    }
+
+    /**
+     * 获取微信支付的http client
+     *
+     * @return
+     */
+    @Bean
+    public CloseableHttpClient getWxPayClient() {
+        WechatPayHttpClientBuilder builder = WechatPayHttpClientBuilder.create()
+                .withMerchant(mchId, mchSerialNo, getPrivateKey())
+                .withValidator(new WechatPay2Validator(getVerifier()));
+        // ... 接下来，你仍然可以通过builder设置各种参数，来配置你的HttpClient
+
+        // 通过WechatPayHttpClientBuilder构造的HttpClient，会自动的处理签名和验签，并进行证书自动更新
+        return builder.build();
     }
 }

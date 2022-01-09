ae6232f9a78dab4e04ea089c9c9a68ff24978c2d
luhan <765478939@qq.com>
2022-01-09 20:36:22 +0800

    fix:支付相关的枚举类、添加工具类

---
+++ payment-atuigu/src/main/java/top/luahncc/payment/domain/enums/wxpay/WxTradeState.java
@@ -0,0 +1,37 @@
+package top.luahncc.payment.domain.enums.wxpay;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+/**
+ * 支付状态枚举
+ */
+@AllArgsConstructor
+@Getter
+public enum WxTradeState {
+
+    /**
+     * 支付成功
+     */
+    SUCCESS("SUCCESS"),
+
+    /**
+     * 未支付
+     */
+    NOTPAY("NOTPAY"),
+
+    /**
+     * 已关闭
+     */
+    CLOSED("CLOSED"),
+
+    /**
+     * 转入退款
+     */
+    REFUND("REFUND");
+
+    /**
+     * 类型
+     */
+    private final String type;
+}
